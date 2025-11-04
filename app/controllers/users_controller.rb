cfd5331e2356d22cbdecc1f44bee24cb43024481
-A <1992anjagi@gmail.com>
2025-11-04 21:28:18 +0300

    frontend signup successful

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,10 @@
 class UsersController < ApplicationController
 skip_before_action :verify_authenticity_token
+
+    def new
+        @user = User.new
+    end
+
     # GET /users
     def index
         @users = User.all
@@ -16,7 +21,8 @@ skip_before_action :verify_authenticity_token
     def create
         @user = User.new(user_params)
         if @user.save
-            render json: @user, status: :created
+            # render json: @user, status: :created
+            redirect_to root_path, notice: "Account created successfully!"
         else
             render json: { errors: @user.errors.full_messages }, status: :unprocessable_entity
         end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  # get "pages/home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -13,5 +14,10 @@ Rails.application.routes.draw do
   # root "posts#index"
 
   resources :users, only: [:index, :show, :create, :update, :destroy]
+
+  root "pages#home" # landing page
+
+  get "/signup", to: "users#new"
+  post "/signup", to: "users#create"
 
 end
